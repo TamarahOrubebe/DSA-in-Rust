1fb112abdfab29155d6e1c96fbe691282f7dec12
root <paulorubebe@yahoo.com>
2024-01-24 15:52:04 +0100

    Create directory for doubly linked list and add structures

---
+++ src/doubly_linked_list/src/main.rs
@@ -0,0 +1,48 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+// The goal here is to use a doublylinkedlist to implement a transaction log
+// say of SQL statements to a database that can be iterated forward/backward to enable recovery of the DB state
+
+
+
+// Create a base Node data structure
+// to help the compiler know the size of the recursive Node type during static analysis, wrap it in a smart
+// pointer and since we would want to mutate it's value the RefCell comes in handy due to interior mutability
+
+type Link = Option<Rc<RefCell<Node>>>;
+#[derive(Debug, Clone)]
+struct Node {
+    value: String,
+    next:  Link,
+    prev: Link
+}
+
+#[derive(Debug, Clone)]
+struct BetterTransactionLog {
+    head: Link,
+    tail: Link,
+    length: u64
+}
+
+
+struct ListIterator {
+    current: List
+}
+
+impl ListIterator {
+    fn new(start_at: Link) -> ListIterator {
+        ListIterator {
+            current: start_at
+        }
+    }
+}
+
+
+impl Iterator for ListIterator {
+    type Item = String;
+
+    fn next(&mut self) -> Option<String> {
+        
+    }
+}
