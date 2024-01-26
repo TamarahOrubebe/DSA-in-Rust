7b65953dd122ba215391b7773ef1dc0ec792f4f1
root <paulorubebe@yahoo.com>
2024-01-26 07:09:06 +0100

    Add functionality to remove item and bubble down

--- src/heap/src/main.rs
+++ src/heap/src/main.rs
@@ -25,13 +25,13 @@ struct MessageNotification {
 
 #[derive(Debug, Clone)]
 struct MessageChecker {
-    pub length: u64,
+    pub length: usize,
     heap: Vec<Box<MessageNotification>>
 }
 
 impl MessageChecker {
     pub fn add(&mut self, notification: MessageNotification) {
-        self.heap.push(Box::new(notifiction));
+        self.heap.push(Box::new(notification));
         self.length += 1;
 
         if self.length > 1 {
@@ -44,17 +44,51 @@ impl MessageChecker {
         
     }
 
-    fn has_more_messages(&self, pos1: u64, pos2: u64) {
+    fn has_more_messages(&self, pos1: usize, pos2: usize) -> bool {
         let a = &self.heap[pos1 - 1];
         let b = &self.heap[pos2 - 1];
         a.no_of_messages >= b.no_of_messages
     }
 
-    fn swap(&mut self, pos1: u64, pos2: u64) {
+    fn swap(&mut self, pos1: usize, pos2: usize) {
+        let index_a = pos1 - 1;
         let index_b = pos2 - 1;
-        let index_a = pos2 - 1;
-        self.heap.swap(a, b);
+        self.heap.swap(index_a, index_b);
     }
 
+    pub fn pop(&mut self) -> Option<MessageNotification> {
+        if self.length > 0 {
+            let elem = self.heap.swap_remove(0);
+            self.length = self.heap.len();
+            let mut i = 1;
+
+            while i * 2 < self.length {
+                let children = (i * 2, i * 2 + 1);
+
+                i = if self.has_more_messages(children.0, children.1) {
+                    if self.has_more_messages(children.0, i) {
+                        self.swap(i, children.0);
+                        children.0
+                    } else {
+                        break;
+                    }
+                } else {
+                    if self.has_more_messages(children.1, i) {
+                        self.swap(i, children.1);
+                        children.1
+                    } else {
+                        break;
+                    }
+                };
+            }
+            Some(*elem)
+        } else {
+            None
+        }
+    }
+}
+
+
+fn main() {
     
 }
